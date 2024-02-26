ad7b09ea1e351ebf299329941da8a23af1f3edc5
rdsarjito <rama.dhaninursarjito@gmail.com>
2024-02-26 16:40:42 +0700

    first commit

---
+++ app/Models/Militaries.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Militaries extends Model
+{
+    use HasFactory;
+
+    protected $table = 'militaries';
+
+    protected $filllable = [
+        'nama',
+        'jenis',
+        'type',
+        'kondisi',
+        'tahun_produksi',
+        'tanggal_perolehan',
+        'matra',
+        'gambar',
+    ];
+}
